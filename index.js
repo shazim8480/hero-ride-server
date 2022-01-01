d13f831ad6fc8494d7811fe3cd700bb1bade2e17
shazim8480 <troublemaker8480@gmail.com>
2022-01-01 23:02:18 +0600

    database saved

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 // for env variable//
 require("dotenv").config();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,4 +29,67 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+client.connect((err) => {
+  const ridersCollection = client.db("hero-ride").collection("riders");
+  const adminsCollection = client.db("hero-ride").collection("admins");
+  console.log("Database connection established!");
+
+  //creating API to add service from admin (Create) //
+  app.post("/addRider", (req, res) => {
+    const newRider = req.body;
+    console.log("adding a new rider", newRider);
+    ridersCollection.insertOne(newRider).then((result) => {
+      res.send(result.insertedCount > 0);
+      console.log(result.insertedCount);
+    });
+  });
+
+  //   // buy now onclick get SERVICE data//
+  app.get("/rider/:id", (req, res) => {
+    const id = ObjectId(req.params.id);
+    ridersCollection.find({ _id: id }).toArray((err, documents) => {
+      console.log(documents);
+      res.send(documents[0]); // must//
+    });
+  });
+
+  //read data for showing all the created services to UI //optional
+  app.get("/riders", (req, res) => {
+    ridersCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  //delete rider by admin////////////
+  app.delete("/deleteRider/:id", (req, res) => {
+    const id = ObjectId(req.params.id);
+    console.log("deleting this", id);
+    ridersCollection.deleteOne({ _id: id }).then((documents) => {
+      res.send(documents.deletedCount > 0);
+      console.log("deleted count", documents.deletedCount);
+    });
+  });
+
+  //////////////////// MAKE ADMIN SECTION//////////////////////////////////
+  app.post("/makeAdmin", (req, res) => {
+    const email = req.body.email;
+    console.log("adding new admin", email);
+    //{email} because it's a part of the whole body//
+    adminsCollection.insertOne({ email }).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // VIP Service only for admins :p //
+  app.post("/isAdmin", (req, res) => {
+    const email = req.body.email;
+    adminsCollection.find({ email: email }).toArray((err, admins) => {
+      console.log(admins);
+      res.send(admins.length > 0);
+    });
+  });
+
+  ///
 });
